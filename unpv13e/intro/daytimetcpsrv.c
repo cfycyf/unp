5838c2b2897f4f7648ac378312a741e8d6715920
cfycyf <cfycyf@yeah.net>
2018-08-14 17:01:48 +0800

    <intro>: modify and make daytimetcp client and server work well
    Signed-off-by: cfycyf <cfycyf@yeah.net>

--- unpv13e/intro/daytimetcpsrv.c
+++ unpv13e/intro/daytimetcpsrv.c
@@ -9,24 +9,24 @@ main(int argc, char **argv)
 	char				buff[MAXLINE];
 	time_t				ticks;
 
-	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port        = htons(13);	/* daytime server */
 
-	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+	bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
-	Listen(listenfd, LISTENQ);
+	listen(listenfd, LISTENQ);
 
 	for ( ; ; ) {
-		connfd = Accept(listenfd, (SA *) NULL, NULL);
+		connfd = accept(listenfd, (SA *) NULL, NULL);
 
         ticks = time(NULL);
         snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-        Write(connfd, buff, strlen(buff));
+        write(connfd, buff, strlen(buff));
 
-		Close(connfd);
+		close(connfd);
 	}
 }
